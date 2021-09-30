29870daf07d0935d8e7eea6a2e43de3167c07d21
aforatik <atik.wowspace@gmail.com>
2021-10-01 01:30:47 +0600

    Added function in (file-processor) to examine file path

--- src/file-processor.py
+++ src/file-processor.py
@@ -1,2 +1,16 @@
-def get_file_info(file):
-    pass
+
+import zipfile
+
+# ========= file path ========
+file_path = "./myfiles/images/new.zip"
+
+
+
+# ========= get filename and extension ==========
+def get_file_info(file_path):
+    file_name = file_path.rpartition('/')[-1]
+    file_extension = file_name.rpartition('.')[-1]
+    file_info_list = [file_name, file_extension]
+    return file_info_list
+
+print(get_file_info(file_path))
