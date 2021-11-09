553987390b5ed485ee98dbc4b9c3a03390fe1633
aforatik <atik.wowspace@gmail.com>
2021-11-09 19:21:09 +0600

    commenting out features for readability

--- src/features.py
+++ src/features.py
@@ -2,22 +2,9 @@ import zipfile
 from tabulate import tabulate
 
 
-def show_children_files_info(archive_file: zipfile.ZipFile):
-    children_file_info = {"file": [], "size": [], 'is Folder': [], 'compress type': [], 'compress size': []}
-    for file in archive_file.filelist:
-        name = file.filename
-        size = file.file_size
-        children_file_info['file'].append(name)
-        children_file_info['size'].append(str(size) + " byte")
-        children_file_info['is Folder'].append(file.is_dir())
-        children_file_info['compress type'].append(file.compress_type)
-        children_file_info['compress size'].append(file.compress_size)
-    print(tabulate(children_file_info, headers='keys', tablefmt='fancy_outline',
-                   showindex=range(1, len(archive_file.filelist) + 1)))
 
 
 def show_base_file_basic_info(archive_file: zipfile.ZipFile):
-
     folder_count = 0
     file_count = 0
     file_name = archive_file.filename.rpartition('/')[-1]
@@ -31,6 +18,37 @@ def show_base_file_basic_info(archive_file: zipfile.ZipFile):
     print("folders in this file : {}".format(folder_count))
     print("number of children files in : {}".format(file_count))
     print("")
+
+
+# ********* 1) Extract all **********
+
+def extract_all_files(archive_file: zipfile.ZipFile, output_directory: str):
+    try:
+        archive_file.extractall(output_directory)
+    except FileExistsError:
+        print("Folder already exists! \nExtracting to default directory...")
+        output_directory = str(archive_file.filename.rpartition('.')[0])
+        archive_file.extractall(output_directory)
+    except Exception as e:
+        print(f'Error! {e.args[1]}')
+    print("all files extracted successfully :)")
+    print("go to -> '{}' to get you files".format(output_directory))
+
+
+# ********* 4) List all file information within this archive *********
+
+def show_children_files_info(archive_file: zipfile.ZipFile):
+    children_file_info = {"file": [], "size": [], 'is Folder': [], 'compress type': [], 'compress size': []}
+    for file in archive_file.filelist:
+        name = file.filename
+        size = file.file_size
+        children_file_info['file'].append(name)
+        children_file_info['size'].append(str(size) + " byte")
+        children_file_info['is Folder'].append(file.is_dir())
+        children_file_info['compress type'].append(file.compress_type)
+        children_file_info['compress size'].append(file.compress_size)
+    print(tabulate(children_file_info, headers='keys', tablefmt='fancy_outline',
+                   showindex=range(1, len(archive_file.filelist) + 1)))
 
 
 def get_files_with_extension(archive_file: zipfile.ZipFile, extension: str):
@@ -66,16 +84,3 @@ def show_search_results(archive_file: zipfile.ZipFile, search_query):
         # ================== If no results found ==================
         search_result_table = {"{} {} files found".format(count_search_result, file_type_string): search_results}
         print(tabulate(search_result_table, headers='keys', tablefmt='fancy_outline'))
-
-
-def extract_all_files(archive_file: zipfile.ZipFile, output_directory: str):
-    try:
-        archive_file.extractall(output_directory)
-    except FileExistsError:
-        print("Folder already exists! \nExtracting to default directory...")
-        output_directory = str(archive_file.filename.rpartition('.')[0])
-        archive_file.extractall(output_directory)
-    except Exception as e:
-        print(f'Error! {e.args[1]}')
-    print("all files extracted successfully :)")
-    print("go to -> '{}' to get you files".format(output_directory))
